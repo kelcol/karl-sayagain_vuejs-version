799ae4fb49c79448ee3bdbdb98887f51843d2c6b
Kelly Colht <colhk@seattleu.edu>
2018-06-10 16:07:25 -0700

    added form validation

--- src/components/Contribute1.vue
+++ src/components/Contribute1.vue
@@ -2,118 +2,108 @@
   <div id="contribute1">
 
     <!--TODO: Move nav elements to own component-->
-    <div class="u-pull-left nav">
+    <ul class="u-pull-left nav">
       <router-link v-bind:to="{name: 'Home'}">
         <li class="button button-primary">Home</li>
       </router-link>
       <router-link v-bind:to="{name: 'About'}">
         <li class="button button-primary">About</li>
       </router-link>
-    </div>
-
-    <!--TODO: Add form validation!-->
-    <!--TODO: Add error and success messaging!-->
-    <section class="container">
-      <h1>Contributing is easy. Just fill out the form below :)</h1>
-      <!-- The above form looks like this -->
-
-      
-      <form id="form" method="post" @submit="onSubmit" v-on:submit.prevent="validateForm">
-        <div class="row">
-          <div class="six columns">
-            <label class="label" label-for="name">Recipe Name</label>
-            <input class="u-full-width" v-model="newRecipe.name" placeholder="Pineapple Upside-down Cake" id="exampleTextInput" type="text">
-          </div>
-          <div class="six columns">
-            <label class="label" label-for="link">Recipe URL</label>
-            <input class="u-full-width" v-model="newRecipe.link" placeholder="https://www.marthastewart.com/343591/pineapple-upside-down-cake"
-              id="exampleUrlInput">
-          </div>
-        </div>
-        <input class="button-primary" value="Submit" type="submit">
-      </form>
-      
-
-      <router-link class="nav-link" v-bind:to="{name: 'Home'}">
-        <button class="button" value="submit">Back to Home</button>
-      </router-link>
-
+    </ul>    
     
+    <div class="container" v-if="!postStatus">
+      <h1>Contributing is easy. Just fill out the form below :)</h1>
+      <form id="form" method="post" v-on:submit.prevent="validateForm">
 
-    <div class="alert alert-dismissible alert-success" v-show="showSuccess">
-      <button type="button" class="close" data-dismiss="alert">&times;</button>
-      <p>
-        <strong>Thank you for signing up!</strong> Please take our new member survey.
-        <br>
-        <br>
-        <router-link to="Survey" class="btn btn-warning" type="button">Click here</router-link>
-      </p>
+        <div class="six columns form-group" v-bind:class="{ 'has-warning': attemptSubmit && missingName }">
+          <label class="label" for="name">Recipe Name</label>
+          <input class="u-full-width" v-model="newRecipe.name" placeholder="Pineapple Upside-down Cake" id="name" type="text">
+          <span id="helpBlock" class="error" v-if="attemptSubmit && missingName">This field is required.</span>
+        </div>
+
+        <div class="six columns form-group" v-bind:class="{ 'has-warning': attemptSubmit && missingLink }">
+          <label class="control-label" for="link">Recipe URL</label>
+          <input id="link" name="link" class="u-full-width" v-model="newRecipe.link" placeholder="https://www.marthastewart.com/343591/pineapple-upside-down-cake"
+            type="text">
+
+          <span id="helpBlock" class="error" v-if="attemptSubmit && missingLink">URL is invalid.</span>
+        </div>
+
+        <button class="btn btn-primary">Submit</button>
+      </form>
     </div>
+    <!-- /col -->
 
-    <div v-show="showError" class="error">
-      <strong style="color: red">Please fill out all fields to submit a recipe.</strong>
-      <button type="button" class="close" data-dismiss="alert">Close</button>
-      
+    <div class="col-md-8" v-if="postStatus">
+
+      <div class="container success">
+        <h2>Thank you for submitting!</h2>
+      </div>
+
     </div>
-    </section>
-
-
   </div>
-</template>
+  <!-- /row -->
 
+
+</template>
 
 <script>
 import firebase from '@/components/firebase-init';
 
-export default {
-  data() {
-    return {
-      showForm: true,
-      showError: false,
-      showSuccess: false,
-      newRecipe: {
-        name: '',
-        link: ''
-      },
-    }
-  },
-  methods: {
-    validateForm: function () {
-      if ((this.name === '') &&
-        (this.link === '')) {
-        console.log("Form is not valid");
-        this.showSuccess = true;
-        this.showError = false;
-      } else {
-        this.showSuccess = false;
-        this.showForm = true;
-        this.showError = true;
-        
-        console.log("Form is not valid");
+  
+  export default {   
+    data () {
+      return {
+        newRecipe: {
+          name: '',
+          link: ''
+        },
+        attemptSubmit: false,
+        postStatus: false
       }
     },
-    onSubmit: function () {
+    computed: {
+        missingName: function () { return this.newRecipe.name === '' },
+        missingLink: function () {
+          return (
+            this.isLink(this.newRecipe.link) === null)
+        },
+  },
+  methods: {
+    isLink: (str) => {
+      let regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      return str.match(regexp);
+    },
+    validateForm: function (event) {
+      this.attemptSubmit = true;
+      if (this.missingName || this.missingLink) {
+          event.preventDefault();
+      } else {
+         this.onSubmit();
+      }       
+    },
+    onSubmit () {      
       firebase.recipesRef.push(this.newRecipe);
       this.newRecipe.name = '';
       this.newRecipe.link = '';
+      this.attemptSubmit = false;
+      this.postStatus = true
       console.log("recipe added");
       this.$ga.event('Contributed Recipe', 'Submit')
-    },
-  }
+      }
+  }   
 }
-
-
 
 </script>
 
 <style scoped>
-h1 {
+.container {
   padding: 10% 10% 3% 10%;
 }
 
 h2 {
   padding: 0px;
-  margin: 0;
+  margin: 5%;
 }
 
 .nav {
@@ -125,10 +115,24 @@ h2 {
 .error {
   position: relative;
   top: 5%;
-  background-color: yellow;
+  color: coral;
+}
+
+.success {
+  color: rgb(48, 173, 90);
+}
+
+.button-contribute {
+  background-color: coral;
+  color: white;
+  border: coral;
+}
+
+.button-contribute:hover {
+  background-color: rgb(253, 167, 136);
+  border: rgb(253, 167, 136);
 }
 
 
-</style>
 
-If you have a moment, my Carl Sagan tribute site could use some clicks! http://kellycolht.net/karl-sayagain_vuejs-version/ The contribute form is currently pretty janky but please do contribute as submissions are saved to firebase and will eventually appear in the project. 
+</style>
--- src/components/Contribute2.vue
+++ src/components/Contribute2.vue
@@ -2,79 +2,108 @@
   <div id="contribute2">
 
     <!--TODO: Move nav elements to own component-->
-    <div class="u-pull-left nav">
+    <ul class="u-pull-left nav">
       <router-link v-bind:to="{name: 'Home'}">
         <li class="button button-primary">Home</li>
       </router-link>
       <router-link v-bind:to="{name: 'About'}">
         <li class="button button-primary">About</li>
       </router-link>
-    </div>
-
-    <!--TODO: Add form validation!-->
-    <!--TODO: Add error and success messaging!-->
-    <section class="container">
+    </ul>    
+    
+    <div class="container" v-if="!postStatus">
       <h1>Contributing is easy. Just fill out the form below :)</h1>
-      <!-- The above form looks like this -->
-      
-      <form id="form" method="post" v-on:submit.prevent="onSubmit">
-        <div class="row">
-          <div class="six columns">
-            <label class="label" label-for="name">Recipe Name</label>
-            <input class="u-full-width" v-model="newRecipe.name" placeholder="Pineapple Upside-down Cake" id="exampleTextInput" type="text">
-          </div>
-          <div class="six columns">
-            <label class="label" label-for="link">Recipe URL</label>
-            <input class="u-full-width" v-model="newRecipe.link" placeholder="https://www.marthastewart.com/343591/pineapple-upside-down-cake"
-              id="exampleUrlInput" type="url">
-          </div>
+      <form id="form" method="post" v-on:submit.prevent="validateForm">
+
+        <div class="six columns form-group" v-bind:class="{ 'has-warning': attemptSubmit && missingName }">
+          <label class="label" for="name">Recipe Name</label>
+          <input class="u-full-width" v-model="newRecipe.name" placeholder="Pineapple Upside-down Cake" id="name" type="text">
+          <span id="helpBlock" class="error" v-if="attemptSubmit && missingName">This field is required.</span>
         </div>
-        <input class="button-primary" value="Submit" type="submit">
+
+        <div class="six columns form-group" v-bind:class="{ 'has-warning': attemptSubmit && missingLink }">
+          <label class="control-label" for="link">Recipe URL</label>
+          <input id="link" name="link" class="u-full-width" v-model="newRecipe.link" placeholder="https://www.marthastewart.com/343591/pineapple-upside-down-cake"
+            type="text">
+
+          <span id="helpBlock" class="error" v-if="attemptSubmit && missingLink">URL is invalid.</span>
+        </div>
+
+        <button class="btn btn-primary">Submit</button>
       </form>
-      <router-link class="nav-link" v-bind:to="{name: 'Home'}">
-        <button class="button" value="submit">Back to Home</button>
-      </router-link>
-    </section>
+    </div>
+    <!-- /col -->
 
+    <div class="col-md-8" v-if="postStatus">
 
+      <div class="container success">
+        <h2>Thank you for submitting!</h2>
+      </div>
+
+    </div>
   </div>
+  <!-- /row -->
+
+
 </template>
 
 <script>
 import firebase from '@/components/firebase-init';
 
-export default {
-  data() {
-    return {
-      newRecipe: {
-        name: '',
-        link: ''
-      },
-      name: 'John'
-    }
+  
+  export default {   
+    data () {
+      return {
+        newRecipe: {
+          name: '',
+          link: ''
+        },
+        attemptSubmit: false,
+        postStatus: false
+      }
+    },
+    computed: {
+        missingName: function () { return this.newRecipe.name === '' },
+        missingLink: function () {
+          return (
+            this.isLink(this.newRecipe.link) === null)
+        },
   },
   methods: {
-    onSubmit: function() {
+    isLink: (str) => {
+      let regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      return str.match(regexp);
+    },
+    validateForm: function (event) {
+      this.attemptSubmit = true;
+      if (this.missingName || this.missingLink) {
+          event.preventDefault();
+      } else {
+         this.onSubmit();
+      }       
+    },
+    onSubmit () {      
       firebase.recipesRef.push(this.newRecipe);
       this.newRecipe.name = '';
-      this.newRecipe.link ='';
+      this.newRecipe.link = '';
+      this.attemptSubmit = false;
+      this.postStatus = true
       console.log("recipe added");
       this.$ga.event('Contributed Recipe', 'Submit')
-    } 
-
-}
+      }
+  }   
 }
 
 </script>
 
 <style scoped>
-h1 {
+.container {
   padding: 10% 10% 3% 10%;
 }
 
 h2 {
   padding: 0px;
-  margin: 0;
+  margin: 5%;
 }
 
 .nav {
@@ -82,6 +111,27 @@ h2 {
   top:2%;
   left: 2%
   }
+
+.error {
+  position: relative;
+  top: 5%;
+  color: coral;
+}
+
+.success {
+  color: rgb(48, 173, 90);
+}
+
+.button-contribute {
+  background-color: coral;
+  color: white;
+  border: coral;
+}
+
+.button-contribute:hover {
+  background-color: rgb(253, 167, 136);
+  border: rgb(253, 167, 136);
+}
 
 
 
